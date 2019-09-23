ec97e54264683486c9581ef5fba2cff7ef7a6660
Alessandro Chitolina <alekitto@gmail.com>
2019-09-23 12:33:22 +0200

    use kcs/metadata 3.1 in metadata annotation processor factory

--- src/DependencyInjection/ElasticaODMExtension.php
+++ src/DependencyInjection/ElasticaODMExtension.php
@@ -2,6 +2,9 @@
 
 namespace Fazland\ODM\ElasticaBundle\DependencyInjection;
 
+use Fazland\ODM\Elastica\Annotation;
+use Fazland\ODM\Elastica\Metadata\Processor;
+use Kcs\Metadata\Loader\Processor\ProcessorFactory;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Console\Application;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -36,5 +39,21 @@ class ElasticaODMExtension extends Extension
 
         $container->getDefinition('fazland_elastica_odm.metadata_cache')
             ->replaceArgument(2, new Parameter('container.build_id'));
+
+        $annotationFactory = $container->findDefinition('fazland_elastica_odm.annotation_processor_factory');
+        if (\method_exists(ProcessorFactory::class, 'registerProcessors')) {
+            $processorRefl = new \ReflectionClass(Processor\IndexProcessor::class);
+            $annotationFactory->addMethodCall('registerProcessors', [ \dirname($processorRefl->getFileName()) ]);
+        } else {
+            $annotationFactory->addMethodCall('registerProcessor', [ Annotation\Document::class, Processor\DocumentProcessor::class ]);
+            $annotationFactory->addMethodCall('registerProcessor', [ Annotation\DocumentId::class, Processor\DocumentIdProcessor::class ]);
+            $annotationFactory->addMethodCall('registerProcessor', [ Annotation\IndexName::class, Processor\IndexNameProcessor::class ]);
+            $annotationFactory->addMethodCall('registerProcessor', [ Annotation\Index::class, Processor\IndexProcessor::class ]);
+            $annotationFactory->addMethodCall('registerProcessor', [ Annotation\TypeName::class, Processor\TypeNameProcessor::class ]);
+            $annotationFactory->addMethodCall('registerProcessor', [ Annotation\Field::class, Processor\FieldProcessor::class ]);
+            if (\class_exists(Annotation\Setting::class)) {
+                $annotationFactory->addMethodCall('registerProcessor', [Annotation\Setting::class, Processor\SettingProcessor::class]);
+            }
+        }
     }
 }
